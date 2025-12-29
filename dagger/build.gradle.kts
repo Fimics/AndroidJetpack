23d6b66893bb593e1d828eac84169c6bfdf5140a
noetix <lpjhblpj@163.com>
2025-12-29 21:42:05 +0800

    dagger 编译通过

--- dagger/build.gradle.kts
+++ dagger/build.gradle.kts
@@ -8,26 +8,17 @@ plugins {
 
 
 android {
-    compileSdk=androids.compileSdkV
-    buildToolsVersion=androids.buildToolsV
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    namespace = "com.mic.dagger"
 
     defaultConfig {
-        applicationId = "com.mic"
-        minSdk=androids.minSdkV
-        targetSdk=androids.targetSdkV
-        versionCode = androids.vCode
-        versionName = androids.vName
+        applicationId = "com.mic.dagger"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
     }
 
-    //签名
-    signingConfigs{
-        register("release"){
-            keyAlias = "fimics"
-            keyPassword ="123456"
-            storeFile =file("../sign.jks")
-            storePassword ="123456"
-        }
-    }
 
     buildTypes {
         getByName("debug") {
@@ -36,7 +27,6 @@ android {
 
         getByName("release") {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -68,10 +58,13 @@ android {
 //https://blog.csdn.net/lfq88/article/details/118222107
 dependencies {
     implementation (fileTree(mapOf("dir" to "libs","include" to listOf("*.jar"))))
-    implementation(Depends.appcompat)
+    implementation(libs.androidx.appcompat)
     //dagger2
-    implementation(Depends.dagger)
-    kapt(Depends.dagger_compiler)
+    implementation(libs.dagger)
+    implementation(libs.dagger.android)
+    implementation(libs.dagger.android.support)
+    implementation(libs.dagger.android.processor)
+    kapt(libs.dagger.compiler)
 
     api(project(mapOf("path" to ":libcore")))
 }
